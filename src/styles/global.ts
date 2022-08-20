f779edef50f3403425dada65ba68e27023adfea3
Mateus Pragana <mateuspragana@gmail.com>
2022-08-20 15:34:20 -0300

    Finalizando aula "Cores & Fontes"

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,10 +8,23 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
 }
 
-body{
-    background: #333;
-    color: #fff
+:focus{
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme["green-500"]};
 }
 
+body{
+    background: ${props => props.theme["gray-900"]};
+    color: ${props => props.theme["gray-300"]};
+}
+
+body, 
+input, 
+button, 
+textarea{
+   font-family: "Roboto", sans-serif;
+   font-weight: 400;
+   font-size: 1rem;
+}
 
 `
