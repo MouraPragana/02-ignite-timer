cfbabb81ff850fb60b0aa0ce4b11cad0fdb649f6
Mateus Pragana <mateuspragana@gmail.com>
2022-08-20 15:59:15 -0300

    Finalizando aula "React Router DOM"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { ThemeProvider } from 'styled-components'
-import { Button } from './Button'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
