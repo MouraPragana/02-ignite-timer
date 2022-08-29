9c0300cdc78fa53a3bb1f8219a730585acd77cac
Mateus Pragana <mateuspragana@gmail.com>
2022-08-29 15:35:45 -0300

    Finalizando aula "Formatação de data"

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 import { CyclesContext } from '../../Contexts/CyclesContext'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
@@ -23,7 +25,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      includeSeconds: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
